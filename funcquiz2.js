19d73b12454b0e5b3c33dbd2cc98d33f5f9832c2
Saumen95 <saumenroy323@gmail.com>
2020-10-11 15:38:53 +0600

    funcquiz2

---
+++ funcquiz2.js
@@ -0,0 +1,10 @@
+function laugh(num) {
+    var ha = '';
+    for (var i = 0; i < num; i = i + 1) {
+        ha += "ha";
+        string = ha + "!";
+    }
+    return string;
+}
+
+console.log(laugh(3));
